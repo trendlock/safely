0fed737f6696e8f176c9669fccfe84b9832dc7cc
rosseji <rosseireland@gmail.com>
2017-10-23 14:14:43 +1100

    add aws safe

--- R/funs.R
+++ R/funs.R
@@ -4,4 +4,6 @@ safe_GET <- purrr::safely(httr::GET)
 safe_html <- purrr::safely(xml2::read_html)
 #' @export
 safe_read_html <- purrr::safely(read_html)
+#' @export
+safe_bucketlist <- purrr::safely(aws.s3::bucketlist)
 
